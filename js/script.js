46008bd3af8904109560fbfd345aa7c32fa3bd35
Katrina H <katrina.hitchcock@gmail.com>
2020-07-28 14:09:25 +0200

    update project section

--- js/script.js
+++ js/script.js
@@ -67,6 +67,23 @@ $(document).ready(function () {
     },
   })
 
+  $('.fancybox')
+    .attr('rel', 'gallery')
+    .fancybox({
+      beforeLoad: function () {
+        var el,
+          id = $(this.element).data('title-id')
+
+        if (id) {
+          el = $('#' + id)
+
+          if (el.length) {
+            this.title = el.html()
+          }
+        }
+      },
+    })
+
   $('#filters a').click(function () {
     $('#filters .current').removeClass('current')
     $(this).addClass('current')

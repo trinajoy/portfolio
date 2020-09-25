ef6d0a3ca845fab478dab25748474430bef5797a
Katrina H <katrina.hitchcock@gmail.com>
2020-09-25 21:41:32 +0200

    update projects

--- js/script.js
+++ js/script.js
@@ -1,73 +1,86 @@
-const premiere = document.getElementById('premiere-preview')
-const fitness = document.getElementById('fitness-preview')
-const music = document.getElementById('music-preview')
+const premiere = document.getElementById("premiere-preview");
+const fitness = document.getElementById("fitness-preview");
+const music = document.getElementById("music-preview");
+const newsletter = document.getElementById("newsletter-preview");
 
-let wrapper = document.querySelector('.project-info-wrapper')
+let wrapper = document.querySelector(".project-info-wrapper");
 
-const enquiry = document.querySelector('.enquiry')
-const form = document.querySelector('.contact-form')
+const enquiry = document.querySelector(".enquiry");
+const form = document.querySelector(".contact-form");
 
 // hover on project - premiere
-premiere.addEventListener('mouseover', function () {
-  premiere.style.background = '#f6f5f5'
-  wrapper.classList.add('visible')
-})
+premiere.addEventListener("mouseover", function () {
+  premiere.style.background = "#f6f5f5";
+  wrapper.classList.add("visible");
+});
 
-premiere.addEventListener('mouseout', function () {
-  premiere.style.backgroundImage = 'url(../img/projects/underbelly-small.jpg)'
-  premiere.style.background = ''
-  wrapper.classList.remove('visible')
-})
+premiere.addEventListener("mouseout", function () {
+  premiere.style.backgroundImage = "url(../img/projects/underbelly.jpg)";
+  premiere.style.background = "";
+  wrapper.classList.remove("visible");
+});
 
 // hover on project - fitness
-fitness.addEventListener('mouseover', function () {
-  fitness.style.background = '#f6f5f5'
-  fitness.querySelector('.project-info-wrapper').classList.add('visible')
-})
+fitness.addEventListener("mouseover", function () {
+  fitness.style.background = "#f6f5f5";
+  fitness.querySelector(".project-info-wrapper").classList.add("visible");
+});
 
-fitness.addEventListener('mouseout', function () {
-  fitness.style.backgroundImage = 'url(../img/projects/pmd-small.jpg)'
-  fitness.style.background = ''
-  fitness.querySelector('.project-info-wrapper').classList.remove('visible')
-})
+fitness.addEventListener("mouseout", function () {
+  fitness.style.backgroundImage = "url(../img/projects/pmd.jpg)";
+  fitness.style.background = "";
+  fitness.querySelector(".project-info-wrapper").classList.remove("visible");
+});
 
 // hover on project - music
-music.addEventListener('mouseover', function () {
-  music.style.background = '#f6f5f5'
-  music.querySelector('.project-info-wrapper').classList.add('visible')
-})
+music.addEventListener("mouseover", function () {
+  music.style.background = "#f6f5f5";
+  music.querySelector(".project-info-wrapper").classList.add("visible");
+});
 
-music.addEventListener('mouseout', function () {
-  music.style.backgroundImage = 'url(../img/projects/music.png)'
-  music.style.background = ''
-  music.querySelector('.project-info-wrapper').classList.remove('visible')
-})
+music.addEventListener("mouseout", function () {
+  music.style.backgroundImage = "url(../img/projects/music.png)";
+  music.style.background = "";
+  music.querySelector(".project-info-wrapper").classList.remove("visible");
+});
+
+// hover on project - newsletter
+newsletter.addEventListener("mouseover", function () {
+  newsletter.style.background = "#f6f5f5";
+  newsletter.querySelector(".project-info-wrapper").classList.add("visible");
+});
+
+newsletter.addEventListener("mouseout", function () {
+  newsletter.style.backgroundImage = "url(../img/projects/newsletter.png)";
+  newsletter.style.background = "";
+  newsletter.querySelector(".project-info-wrapper").classList.remove("visible");
+});
 
 // Event listeners
-form.addEventListener('submit', function (e) {
+form.addEventListener("submit", function (e) {
   // e.preventDefault()
 
   //   checkEmail(email)
-  const form = (document.getElementById('enquiry').className =
-    'contact-form sent')
+  const form = (document.getElementById("enquiry").className =
+    "contact-form sent");
 
-  console.log('submit clicked')
-})
+  console.log("submit clicked");
+});
 
-$(window).on('load', function () {
-  $('.loader .inner').fadeOut(500, function () {
-    $('.loader').fadeOut(750)
-  })
-})
+$(window).on("load", function () {
+  $(".loader .inner").fadeOut(500, function () {
+    $(".loader").fadeOut(750);
+  });
+});
 
 $(document).ready(function () {
-  $('#slides').superslides({
-    animation: 'fade',
+  $("#slides").superslides({
+    animation: "fade",
     play: 5000,
     pagination: false,
-  })
+  });
 
-  $('.owl-carousel').owlCarousel({
+  $(".owl-carousel").owlCarousel({
     loop: true,
     items: 4,
     responsive: {
@@ -84,22 +97,22 @@ $(document).ready(function () {
         items: 4,
       },
     },
-  })
+  });
 
-  const nav = $('#navigation')
-  const navTop = nav.offset().top
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
 
-  $(window).on('scroll', stickyNavigation)
+  $(window).on("scroll", stickyNavigation);
 
   function stickyNavigation() {
-    var body = $('body')
+    var body = $("body");
 
     if ($(window).scrollTop() >= navTop) {
-      body.css('padding-top', nav.outerHeight() + 'px')
-      body.addClass('fixedNav')
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
     } else {
-      body.css('padding-top', 0)
-      body.removeClass('fixedNav')
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
     }
   }
 
@@ -107,40 +120,40 @@ $(document).ready(function () {
 
   function myFunction(media) {
     if (media.matches) {
-      $('#navigation li a').click(function (e) {
-        e.preventDefault()
-        var targetElement = $(this).attr('href')
-        var targetPosition = $(targetElement).offset().top
-        $('html').animate({ scrollTop: targetPosition - 190 }, 'slow')
-      })
+      $("#navigation li a").click(function (e) {
+        e.preventDefault();
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html").animate({ scrollTop: targetPosition - 190 }, "slow");
+      });
     } else {
-      $('#navigation li a').click(function (e) {
-        e.preventDefault()
-        var targetElement = $(this).attr('href')
-        var targetPosition = $(targetElement).offset().top
-        $('html').animate({ scrollTop: targetPosition - 50 }, 'slow')
-      })
+      $("#navigation li a").click(function (e) {
+        e.preventDefault();
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html").animate({ scrollTop: targetPosition - 50 }, "slow");
+      });
     }
   }
 
-  var media = window.matchMedia('(max-width: 900px)')
-  myFunction(media) // Call listener function at run time
-  media.addListener(myFunction) // Attach listener function on state changes
+  var media = window.matchMedia("(max-width: 900px)");
+  myFunction(media); // Call listener function at run time
+  media.addListener(myFunction); // Attach listener function on state changes
 
-  $('#filters a').click(function () {
-    $('#filters .current').removeClass('current')
-    $(this).addClass('current')
+  $("#filters a").click(function () {
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
 
-    var selector = $(this).attr('data-filter')
+    var selector = $(this).attr("data-filter");
 
-    $('.projects').isotope({
+    $(".projects").isotope({
       filter: selector,
       animationOptions: {
         duration: 1500,
-        easing: 'linear',
+        easing: "linear",
         queue: false,
       },
-    })
-    return false
-  })
-})
+    });
+    return false;
+  });
+});

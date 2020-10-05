642373b11f2462d3e1f2cc468826e40362146ada
Katrina H <katrina.hitchcock@gmail.com>
2020-10-05 20:38:45 +0200

    refactor mouse event listeners, remove unused packages

--- js/script.js
+++ js/script.js
@@ -2,59 +2,35 @@ const premiere = document.getElementById("premiere-preview");
 const fitness = document.getElementById("fitness-preview");
 const music = document.getElementById("music-preview");
 const newsletter = document.getElementById("newsletter-preview");
-
+const breakout = document.getElementById("breakout-preview");
 let wrapper = document.querySelector(".project-info-wrapper");
+
+projectNames = [premiere, fitness, music, newsletter, breakout];
+
+// loop through project names and generate mouseover & mouseout effects
+function createProjects() {
+  projectNames.forEach((project) => {
+    project.addEventListener("mouseover", function () {
+      project.style.background = "#f6f5f5";
+      project.querySelector(".project-info-wrapper").classList.add("visible");
+    });
+  });
+
+  projectNames.forEach((project) => {
+    project.addEventListener("mouseout", function () {
+      project.style.backgroundImage = `url(../img/projects/${project}.jpg)`;
+      project.style.background = "";
+      project
+        .querySelector(".project-info-wrapper")
+        .classList.remove("visible");
+    });
+  });
+}
+
+createProjects();
 
 const enquiry = document.querySelector(".enquiry");
 const form = document.querySelector(".contact-form");
-
-// hover on project - premiere
-premiere.addEventListener("mouseover", function () {
-  premiere.style.background = "#f6f5f5";
-  wrapper.classList.add("visible");
-});
-
-premiere.addEventListener("mouseout", function () {
-  premiere.style.backgroundImage = "url(../img/projects/underbelly.jpg)";
-  premiere.style.background = "";
-  wrapper.classList.remove("visible");
-});
-
-// hover on project - fitness
-fitness.addEventListener("mouseover", function () {
-  fitness.style.background = "#f6f5f5";
-  fitness.querySelector(".project-info-wrapper").classList.add("visible");
-});
-
-fitness.addEventListener("mouseout", function () {
-  fitness.style.backgroundImage = "url(../img/projects/pmd.jpg)";
-  fitness.style.background = "";
-  fitness.querySelector(".project-info-wrapper").classList.remove("visible");
-});
-
-// hover on project - music
-music.addEventListener("mouseover", function () {
-  music.style.background = "#f6f5f5";
-  music.querySelector(".project-info-wrapper").classList.add("visible");
-});
-
-music.addEventListener("mouseout", function () {
-  music.style.backgroundImage = "url(../img/projects/music.png)";
-  music.style.background = "";
-  music.querySelector(".project-info-wrapper").classList.remove("visible");
-});
-
-// hover on project - newsletter
-newsletter.addEventListener("mouseover", function () {
-  newsletter.style.background = "#f6f5f5";
-  newsletter.querySelector(".project-info-wrapper").classList.add("visible");
-});
-
-newsletter.addEventListener("mouseout", function () {
-  newsletter.style.backgroundImage = "url(../img/projects/newsletter.png)";
-  newsletter.style.background = "";
-  newsletter.querySelector(".project-info-wrapper").classList.remove("visible");
-});
 
 // Event listeners
 form.addEventListener("submit", function (e) {
@@ -78,25 +54,6 @@ $(document).ready(function () {
     animation: "fade",
     play: 5000,
     pagination: false,
-  });
-
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    items: 4,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      480: {
-        items: 2,
-      },
-      768: {
-        items: 3,
-      },
-      938: {
-        items: 4,
-      },
-    },
   });
 
   const nav = $("#navigation");
